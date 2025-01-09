f8da11bd67ed4c6f5ad3608cb9852a98eb910a80
uomer <uomer@students.cs.ubc.ca>
2025-01-08 18:05:34 -0800

    Fix: ESLint issue in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+/* eslint-disable */
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import {defineConfig} from "vite";
+import react from '@vitejs/plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
